344d014a738becbf8d860c1d1b7dcd1d260ac27c
DNEGEL3125 <2839212140@qq.com>
2025-02-06 10:07:18 +0800

    refactor(utils::terminal): remove needless `return`

--- src/utils/terminal.rs
+++ src/utils/terminal.rs
@@ -1,15 +1,19 @@
 pub fn get_terminal_total_rows() -> u16 {
     let terminal_size = crossterm::terminal::size().expect("Doesn't support get height");
-    return terminal_size.1;
+    terminal_size.1
 }
 
 pub fn get_terminal_total_cols() -> u16 {
     let terminal_size = crossterm::terminal::size().expect("Doesn't support get width");
-    return terminal_size.0;
+    terminal_size.0
 }
 
 pub fn restore_terminal() -> Result<(), std::io::Error> {
     use crossterm::*;
-    execute!(std::io::stdout(), terminal::LeaveAlternateScreen, cursor::Show)?;
+    execute!(
+        std::io::stdout(),
+        terminal::LeaveAlternateScreen,
+        cursor::Show
+    )?;
     Ok(())
 }
