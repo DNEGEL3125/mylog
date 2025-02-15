c6715416a37f615cad57f4a4d7259dacdab5aee7
DNEGEL3125 <2839212140@qq.com>
2025-02-15 14:02:08 +0800

    test(log_pager::command): `from_str`

--- src/log_pager/command.rs
+++ src/log_pager/command.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum Command {
     ShowDate,
     SetDate(String),
@@ -18,5 +19,22 @@ impl Command {
             };
         }
         Command::None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Command;
+    #[test]
+    fn test_command_from_str() {
+        assert_eq!(
+            Command::from_str("date 2025-2-15"),
+            Command::SetDate("2025-2-15".to_owned())
+        );
+        assert_eq!(
+            Command::from_str("date 2021-12-5"),
+            Command::SetDate("2021-12-5".to_owned())
+        );
+        assert_eq!(Command::from_str("date"), Command::ShowDate);
     }
 }
